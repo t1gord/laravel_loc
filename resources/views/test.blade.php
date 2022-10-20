30e07eaf1afbc48477110b36a5e5c811dc8798c7
t1gord <ginga444@gmail.com>
2022-10-20 22:37:49 +0300

    Test Template

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,11 +1,11 @@
 <html lang="en"><head>
     <meta charset="utf-8">
-    <title>Silicon | Portfolio Grid View</title>
+    <title>Lilya Neko</title>
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Silicon - Multipurpose Technology Bootstrap Template">
-    <meta name="keywords" content="bootstrap, business, creative agency, mobile app showcase, saas, fintech, finance, online courses, software, medical, conference landing, services, e-commerce, shopping cart, multipurpose, shop, ui kit, marketing, seo, landing, blog, portfolio, html5, css3, javascript, gallery, slider, touch, creative">
-    <meta name="author" content="Createx Studio">
+    <meta name="description" content="Lilya Neko - художник">
+    <meta name="keywords" content="">
+    <meta name="author" content="Lilya Neko">
 
     <!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -108,7 +108,7 @@
     </style>
 
     <!-- Theme mode -->
-    <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-TXTBFKF5EW&amp;l=dataLayer&amp;cx=c"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-WKV3GT5"></script><script>
+    <script>
       let mode = window.localStorage.getItem('mode'),
           root = document.getElementsByTagName('html')[0];
       if (mode !== null && mode === 'dark') {
@@ -130,28 +130,14 @@
         };
       })();
     </script>
-
-    <!-- Google Tag Manager -->
-    <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WKV3GT5');
-    </script>
   </head>
 
 
   <!-- Body -->
   <body style="">
-    
-    <!-- Google Tag Manager (noscript)-->
-    <noscript>
-      <iframe src="//www.googletagmanager.com/ns.html?id=GTM-WKV3GT5" height="0" width="0" style="display: none; visibility: hidden;"></iframe>
-    </noscript>
 
     <!-- Page loading spinner -->
-    
+
 
 
     <!-- Page wrapper for sticky footer -->
@@ -164,8 +150,8 @@
       <header class="header navbar navbar-expand-lg bg-light navbar-sticky">
         <div class="container px-3">
           <a href="index.html" class="navbar-brand pe-3">
-            <img src="assets/img/logo.svg" width="47" alt="Silicon">
-            Silicon
+            <img src="assets/img/logo.svg" width="47" alt="Lilya Neko">
+              Lilya Neko
           </a>
           <div id="navbarNav" class="offcanvas offcanvas-end">
             <div class="offcanvas-header border-bottom">
@@ -175,122 +161,78 @@
             <div class="offcanvas-body">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Landings</a>
+                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Illustrations</a>
                   <div class="dropdown-menu p-0">
                     <div class="d-lg-flex">
-                      <div class="mega-dropdown-column bg-position-center bg-repeat-0 bg-size-cover rounded-3 rounded-end-0" style="background-image: url(assets/img/landings.jpg); margin: -1px;"></div>
+                      {{--<div class="mega-dropdown-column bg-position-center bg-repeat-0 bg-size-cover rounded-3 rounded-end-0" style="background-image: url(assets/img/landings.jpg); margin: -1px;"></div>--}}
                       <div class="mega-dropdown-column pt-lg-3 pb-lg-4">
                         <ul class="list-unstyled mb-0">
-                          <li><a href="index.html" class="dropdown-item">Template Intro Page</a></li>
-                          <li><a href="landing-mobile-app-showcase-v1.html" class="dropdown-item">Mobile App Showcase v.1</a></li>
-                          <li><a href="landing-mobile-app-showcase-v2.html" class="dropdown-item">Mobile App Showcase v.2</a></li>
-                          <li><a href="landing-startup.html" class="dropdown-item d-flex align-items-center">Startup<span class="badge bg-success ms-2">New</span></a></li>
-                          <li><a href="landing-saas-v1.html" class="dropdown-item">SaaS v.1</a></li>
-                          <li><a href="landing-saas-v2.html" class="dropdown-item">SaaS v.2</a></li>
-                          <li><a href="landing-saas-v3.html" class="dropdown-item">SaaS v.3</a></li>
-                          <li><a href="landing-financial.html" class="dropdown-item">Financial Consulting</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 1</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 2</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 3</a></li>
+                          <li><a href="#" class="dropdown-item d-flex align-items-center">Illustrations <span class="badge bg-success ms-2">New</span></a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 4</a></li>
                         </ul>
                       </div>
                       <div class="mega-dropdown-column pt-lg-3 pb-lg-4">
                         <ul class="list-unstyled mb-0">
-                          <li><a href="landing-online-courses.html" class="dropdown-item">Online Courses</a></li>
-                          <li><a href="landing-medical.html" class="dropdown-item">Medical</a></li>
-                          <li><a href="landing-software-company.html" class="dropdown-item">IT (Software) Company</a></li>
-                          <li><a href="landing-conference.html" class="dropdown-item">Conference</a></li>
-                          <li><a href="landing-digital-agency.html" class="dropdown-item">Digital Agency</a></li>
-                          <li><a href="landing-blog.html" class="dropdown-item">Blog Homepage</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 5</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 6</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 7</a></li>
+                          <li><a href="#" class="dropdown-item">Illustrations 8</a></li>
                         </ul>
                       </div>
                     </div>
                   </div>
                 </li>
                 <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
+                  <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Painting</a>
                   <div class="dropdown-menu">
                     <div class="d-lg-flex pt-lg-3">
                       <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">About</h6>
+                        <h6 class="px-3 mb-2">Oil</h6>
                         <ul class="list-unstyled mb-3">
-                          <li><a href="about-v1.html" class="dropdown-item py-1">About v.1</a></li>
-                          <li><a href="about-v2.html" class="dropdown-item py-1">About v.2</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting v.1</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting v.2</a></li>
                         </ul>
-                        <h6 class="px-3 mb-2">Blog</h6>
+                        <h6 class="px-3 mb-2">Pencil</h6>
                         <ul class="list-unstyled mb-3">
-                          <li><a href="blog-list-with-sidebar.html" class="dropdown-item py-1">List View with Sidebar</a></li>
-                          <li><a href="blog-grid-with-sidebar.html" class="dropdown-item py-1">Grid View with Sidebar</a></li>
-                          <li><a href="blog-list-no-sidebar.html" class="dropdown-item py-1">List View no Sidebar</a></li>
-                          <li><a href="blog-grid-no-sidebar.html" class="dropdown-item py-1">Grid View no Sidebar</a></li>
-                          <li><a href="blog-simple-feed.html" class="dropdown-item py-1">Simple Feed</a></li>
-                          <li><a href="blog-single.html" class="dropdown-item py-1">Single Post</a></li>
-                          <li><a href="blog-podcast.html" class="dropdown-item py-1">Podcast</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 1</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 2</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 3</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 4</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 5</a></li>
                         </ul>
                       </div>
                       <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">Portfolio</h6>
+                        <h6 class="px-3 mb-2">Watercolor</h6>
                         <ul class="list-unstyled mb-3">
-                          <li><a href="portfolio-grid.html" class="dropdown-item py-1">Grid View</a></li>
-                          <li><a href="portfolio-list.html" class="dropdown-item py-1">List View</a></li>
-                          <li><a href="portfolio-slider.html" class="dropdown-item py-1">Slider View</a></li>
-                          <li><a href="portfolio-courses.html" class="dropdown-item py-1">Courses</a></li>
-                          <li><a href="portfolio-single-project.html" class="dropdown-item py-1">Single Project</a></li>
-                          <li><a href="portfolio-single-course.html" class="dropdown-item py-1">Single Course</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Services</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="services-v1.html" class="dropdown-item py-1">Services v.1</a></li>
-                          <li><a href="services-v2.html" class="dropdown-item py-1">Services v.2</a></li>
-                          <li><a href="services-single-v1.html" class="dropdown-item py-1">Service Details v.1</a></li>
-                          <li><a href="services-single-v2.html" class="dropdown-item py-1">Service Details v.2</a></li>
-                        </ul>
-                      </div>
-                      <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">Pricing</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="pricing.html" class="dropdown-item py-1">Pricing Page</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Contacts</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="contacts-v1.html" class="dropdown-item py-1">Contacts v.1</a></li>
-                          <li><a href="contacts-v2.html" class="dropdown-item py-1">Contacts v.2</a></li>
-                          <li><a href="contacts-v3.html" class="dropdown-item py-1">Contacts v.3</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Specialty</h6>
-                        <ul class="list-unstyled">
-                          <li><a href="404-v1.html" class="dropdown-item py-1">404 Error v.1</a></li>
-                          <li><a href="404-v2.html" class="dropdown-item py-1">404 Error v.2</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 1</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 2</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 3</a></li>
+                          <li><a href="#" class="dropdown-item py-1">Painting 4</a></li>
                         </ul>
                       </div>
                     </div>
                   </div>
                 </li>
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Account</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="account-details.html" class="dropdown-item">Account Details</a></li>
-                    <li><a href="account-security.html" class="dropdown-item">Security</a></li>
-                    <li><a href="account-notifications.html" class="dropdown-item">Notifications</a></li>
-                    <li><a href="account-messages.html" class="dropdown-item">Messages</a></li>
-                    <li><a href="account-saved-items.html" class="dropdown-item">Saved Items</a></li>
-                    <li><a href="account-collections.html" class="dropdown-item">My Collections</a></li>
-                    <li><a href="account-payment.html" class="dropdown-item">Payment Details</a></li>
-                    <li><a href="account-signin.html" class="dropdown-item">Sign In</a></li>
-                    <li><a href="account-signup.html" class="dropdown-item">Sign Up</a></li>
-                  </ul>
+                  <li class="nav-item">
+                  <a href="#" class="nav-link">About Me</a>
                 </li>
                 <li class="nav-item">
-                  <a href="components/typography.html" class="nav-link">UI Kit</a>
+                  <a href="#" class="nav-link">Contacts</a>
                 </li>
                 <li class="nav-item">
-                  <a href="docs/getting-started.html" class="nav-link">Docs</a>
+                  <a href="#" class="nav-link">Blog</a>
                 </li>
               </ul>
             </div>
-            <div class="offcanvas-header border-top">
+{{--            <div class="offcanvas-header border-top">
               <a href="https://themes.getbootstrap.com/product/silicon-business-technology-template-ui-kit/" class="btn btn-primary w-100" target="_blank" rel="noopener">
                 <i class="bx bx-cart fs-4 lh-1 me-1"></i>
                 &nbsp;Buy now
               </a>
-            </div>      
+            </div>--}}
           </div>
           <div class="form-check form-switch mode-switch pe-lg-1 ms-auto me-4" data-bs-toggle="mode">
             <input type="checkbox" class="form-check-input" id="theme-mode">
@@ -300,13 +242,13 @@
           <button type="button" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <a href="https://themes.getbootstrap.com/product/silicon-business-technology-template-ui-kit/" class="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex" target="_blank" rel="noopener">
+          {{--<a href="https://themes.getbootstrap.com/product/silicon-business-technology-template-ui-kit/" class="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex" target="_blank" rel="noopener">
             <i class="bx bx-cart fs-5 lh-1 me-1"></i>
             &nbsp;Buy now
-          </a>
+          </a>--}}
         </div>
       </header>
-              
+
 
       <!-- Breadcrumb -->
       <nav class="container py-4 mb-lg-2 mt-lg-3" aria-label="breadcrumb">
@@ -511,11 +453,11 @@
         <div class="row pb-5">
           <div class="col-lg-4 col-md-6">
             <div class="navbar-brand text-dark p-0 me-0 mb-3 mb-lg-4">
-              <img src="assets/img/logo.svg" width="47" alt="Silicon">
-              Silicon
+              <img src="assets/img/logo.svg" width="47" alt="Lilya Neko">
+                Lilya Neko
             </div>
             <p class="fs-sm pb-lg-3 mb-4">Proin ipsum pharetra, senectus eget scelerisque varius pretium platea velit. Lacus, eget eu vitae nullam proin turpis etiam mi sit. Non feugiat feugiat egestas nulla nec. Arcu tempus, eget elementum dolor ullamcorper sodales ultrices eros.</p>
-            <form class="needs-validation" novalidate="">
+{{--            <form class="needs-validation" novalidate="">
               <label for="subscr-email" class="form-label">Subscribe to our newsletter</label>
               <div class="input-group">
                 <input type="email" id="subscr-email" class="form-control rounded-start ps-5" placeholder="Your email" required="">
@@ -523,7 +465,7 @@
                 <div class="invalid-tooltip position-absolute top-100 start-0">Please provide a valid email address.</div>
                 <button type="submit" class="btn btn-primary">Subscribe</button>
               </div>
-            </form>
+            </form>--}}
           </div>
           <div class="col-xl-6 col-lg-7 col-md-5 offset-xl-2 offset-md-1 pt-4 pt-md-1 pt-lg-0">
             <div id="footer-links" class="row">
@@ -551,8 +493,8 @@
                 </h6>
                 <div id="social-links" class="collapse d-lg-block" data-bs-parent="#footer-links">
                   <ul class="nav flex-column mb-2 mb-lg-0">
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Facebook</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">LinkedIn</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Vkontakte</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Instagram</a></li>
                     <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Twitter</a></li>
                     <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Behance</a></li>
                   </ul>
@@ -566,8 +508,7 @@
           </div>
         </div>
         <p class="nav d-block fs-xs text-center text-md-start pb-2 pb-lg-0 mb-0">
-          © All rights reserved. Made by 
-          <a class="nav-link d-inline-block p-0" href="#" target="_blank" rel="noopener">Createx Studio</a>
+          © All rights reserved.
         </p>
       </div>
     </footer>
@@ -587,5 +528,5 @@
 
     <!-- Main Theme Script -->
     <script src="js/theme.min.js"></script>
-  
+
 </body></html>
