e18faea05d55197879b5d3b68952d4b30c1d49fa
t1gord <ginga444@gmail.com>
2023-01-29 00:16:38 +0300

    Update Laravel to 9.0

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PortfolioCategoryController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,18 +8,12 @@ Route::get('/', function () {
 
 Route::get('/test-portfolio', function () {
     return view('test');
-});
+})->name('portfolio');
 
 Route::get('/test-art', function () {
     return view('test-art');
-});
-
-Route::resource('portfolio', PortfolioCategoryController::class);
-
-Route::get('/portfolio/category/{alias}', function () {
-    return view('test-art');
-});
+})->name('test-art');
 
 Route::get('/test', function () {
     return Route::currentRouteName();
-});
+})->name('test');
