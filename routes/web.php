2548c2c3caec77fd1ebe8529db31ea304e6bf10b
t1gord <ginga444@gmail.com>
2023-01-31 22:09:24 +0300

    Rebuilding beta

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PortfolioCategoryController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.layout');
 });
-
-Route::get('test-portfolio', function () {
-    return view('test');
-})->name('portfolio');
-
-Route::get('test-page', [PortfolioCategoryController::class, 'index']);
-
-Route::get('test-art', function () {
-    return view('test-art');
-})->name('test-art');
-
-Route::get('test', function () {
-    return Route::currentRouteName();
-})->name('test');
