431d4dcae02bb0e0f1488a09f8124d90c07abeb7
t1gord <ginga444@gmail.com>
2023-02-02 00:16:28 +0300

    Настройка отношений между Артами и Категориями. Вывод списка категорий под Артом.

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use \App\Http\Controllers\ArtController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
-
-Route::get('/', [ArtController::class, 'home']);
+Route::get('/', [ArtController::class, 'home'])->name('home');
 
 Route::get('single', function () {
     return view('pages.single');
